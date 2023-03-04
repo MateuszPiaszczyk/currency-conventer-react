d5501cb57cf96b8fb444c84eeb2aac16c796c8be
Mateusz Piaszczyk <mateuszpiaszczyk91@gmail.com>
2023-03-04 14:35:54 +0100

    remove container and replace to App

--- src/App/Container/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-export const Container = ({ children }) => (
-    <div className="container">
-        {children}
-    </div>
-);
-
-
--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,8 +1,7 @@
-import "./style.css"
-import { Container } from "../Container"
-import { currencies } from "../currencies"
-import { useState } from "react"
-import { Result } from "./Result"
+import "./style.css";
+import { currencies } from "../currencies";
+import { useState } from "react";
+import { Result } from "./Result";
 
 export const Form = ({ calculateResult, result }) => {
     const [amount, setAmount] = useState(" ");
@@ -16,69 +15,66 @@ export const Form = ({ calculateResult, result }) => {
     }
     return (
 
-        <Container>
+        <form className="form" onSubmit={onSubmit} >
 
-            <form className="form" onSubmit={onSubmit} >
+            <fieldset className="form__fieldset">
+                <h1 className="heading">
+                    Currency Conventer React app
+                </h1>
+                <legend className="form__legend">
+                    Przelicznik walut
+                </legend>
+                <p>
+                    <label>
+                        <span className="form__labelText">
+                            Kwota w zł
+                            <span
+                                className="form__field form__field--important">
+                                *
+                            </span>:
+                        </span>
+                        <input
+                            value={amount}
+                            onChange={({ target }) => setAmount(target.value)}
+                            placeholder="Wpisz kwotę w zł"
+                            className="form__field"
+                            min="0.01"
+                            type="number"
+                            required
+                            autoFocus
+                            step="0.01" />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <span className="form__labelText">
+                            Waluta
+                        </span>
+                        <select
+                            className="form__field"
+                            value={currency}
+                            onChange={onSelectChange}
+                        >
+                            {currencies.map((currency => (
+                                <option
+                                    key={currency.shortName}
+                                    value={currency.shortName}
+                                >
+                                    {currency.flagIcon} - {currency.shortName} - {currency.fullName}
+                                </option>
+                            )))};
+                        </select>
+                    </label>
+                </p>
 
-                <fieldset className="form__fieldset">
-                    <h1 className="heading">
-                        Currency Conventer React app
-                    </h1>
-                    <legend className="form__legend">
-                        Przelicznik walut
-                    </legend>
-                    <p>
-                        <label>
-                            <span className="form__labelText">
-                                Kwota w zł
-                                <span
-                                    className="form__field form__field--important">
-                                    *
-                                </span>:
-                            </span>
-                            <input
-                                value={amount}
-                                onChange={({ target }) => setAmount(target.value)}
-                                placeholder="Wpisz kwotę w zł"
-                                className="form__field"
-                                min="0.01"
-                                type="number"
-                                required
-                                autoFocus
-                                step="0.01" />
-                        </label>
-                    </p>
-                    <p>
-                        <label>
-                            <span className="form__labelText">
-                                Waluta
-                            </span>
-                            <select
-                                className="form__field"
-                                value={currency}
-                                onChange={onSelectChange}
-                            >
-                                {currencies.map((currency => (
-                                    <option
-                                        key={currency.shortName}
-                                        value={currency.shortName}
-                                    >
-                                        {currency.flagIcon} - {currency.shortName} - {currency.fullName}
-                                    </option>
-                                )))};
-                            </select>
-                        </label>
-                    </p>
+                <button className="form__button">
+                    Przelicz Walutę
+                </button>
 
-                    <button className="form__button">
-                        Przelicz Walutę
-                    </button>
-
-                    <p className="form__info"> Kursy Walut na podstawie danych z Tabeli nr 008/A/NBP/2023 z dnia 2023-01-12 </p>
-                    <Result result={result} />
-                </fieldset>
-            </form>
-        </Container>
+                <p className="form__info"> Kursy Walut na podstawie danych z Tabeli nr 008/A/NBP/2023 z dnia 2023-01-12 </p>
+                <Result result={result} />
+            </fieldset>
+        </form>
     );
 };
 
